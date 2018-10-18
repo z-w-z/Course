2686185fb402da34c13789a6fc571a1dc6b45c1f
17Wang <1520166121@qq.com>
2018-10-18 21:08:19 +0800

    修改Controller里的添加信息
    暂时先不要写Controller里的东西

--- project/src/main/java/com/course/demo/Controller/OrderPrivPageController.java
+++ project/src/main/java/com/course/demo/Controller/OrderPrivPageController.java
@@ -3,10 +3,7 @@ package com.course.demo.Controller;
 import com.course.demo.Entity.OrderPrivateEntity;
 import com.course.demo.Service.OrderPrivService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/AdminPage/OrderPrivPage")
@@ -15,9 +12,9 @@ public class OrderPrivPageController {
     OrderPrivService orderPrivService;
 
     //添加一个私教课订单
-    @RequestMapping(value = "/addOrderPrivPage")
+    @RequestMapping(value = "/addOrderPrivate",method = RequestMethod.POST)
     @ResponseBody
-    public boolean addOrderPriv(@RequestBody OrderPrivateEntity orderPrivateEntity){
+    public boolean addOrderPrivate(@RequestBody OrderPrivateEntity orderPrivateEntity){
         return orderPrivService.addOrderPriv(orderPrivateEntity);
     }
 }
--- project/src/main/java/com/course/demo/Controller/OrderPubPageController.java
+++ project/src/main/java/com/course/demo/Controller/OrderPubPageController.java
@@ -3,10 +3,7 @@ package com.course.demo.Controller;
 import com.course.demo.Entity.OrderPublicEntity;
 import com.course.demo.Service.OrderPubService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/AdminPage/OrderPubPage")
@@ -15,7 +12,7 @@ public class OrderPubPageController {
     OrderPubService orderPubService;
 
     //添加一个公共课订单
-    @RequestMapping(value = "/addOrderPubPage")
+    @RequestMapping(value = "/addOrderPublic",method = RequestMethod.POST)
     @ResponseBody
     public boolean addOrderPub(@RequestBody OrderPublicEntity orderPublicEntity){
         return orderPubService.addOrderPub(orderPublicEntity);
